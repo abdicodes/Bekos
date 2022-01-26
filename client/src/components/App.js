c0d21b4086871e8907043278ce9d698062516c26
Abdirahman Suldaan <suldaan.89@hotmail.com>
2022-01-26 05:48:43 +0200

    login with router

---
+++ client/src/components/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Signup from "./Signup";
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "../contexts/AuthContext";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ContextProvider } from "../contexts/SocketContext";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
+import PrivateRoute from "./PrivateRoute";
+import ForgotPassword from "./ForgotPassword";
+import UpdateProfile from "./UpdateProfile";
+import VideoApi from "./VideoApi";
+
+function App() {
+  return (
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <ContextProvider>
+            <AuthProvider>
+              <Switch>
+                <PrivateRoute exact path="/" component={Dashboard} />
+                <PrivateRoute
+                  path="/update-profile"
+                  component={UpdateProfile}
+                />
+                <PrivateRoute path="/video" component={VideoApi} />
+                <Route path="/signup" component={Signup} />
+                <Route path="/login" component={Login} />
+                <Route path="/forgot-password" component={ForgotPassword} />
+              </Switch>
+            </AuthProvider>
+          </ContextProvider>
+        </Router>
+      </div>
+    </Container>
+  );
+}
+
+export default App;
--- client/src/components/Notification.jsx
+++ client/src/components/Notification.jsx
@@ -1,23 +1,24 @@
-import React, { useContext } from 'react';
-import { Button } from '@material-ui/core';
+import React, { useContext } from "react";
+import { Button } from "@material-ui/core";
 
-import { SocketContext } from '../SocketContext';
+import { SocketContext } from "../contexts/SocketContext";
 
 const Notification = () => {
-    const { answerCall, call, callAccepted } = useContext(SocketContext);
-    
+  const { answerCall, call, callAccepted } = useContext(SocketContext);
+
   return (
-      <>
-        {call.isReceivedCall && !callAccepted &&(
-            /* TODO: CSS must be moved in Notification.css */
-            <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <h1>{ call.name } is calling: </h1>
-                <Button variant="contained" color="primary" onClick={answerCall}>
-                    Answer
-                </Button>
-            </div>
-        )};
-      </>
+    <>
+      {call.isReceivedCall && !callAccepted && (
+        /* TODO: CSS must be moved in Notification.css */
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <h1>{call.name} is calling: </h1>
+          <Button variant="contained" color="primary" onClick={answerCall}>
+            Answer
+          </Button>
+        </div>
+      )}
+      ;
+    </>
   );
 };
 
